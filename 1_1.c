958fa60202e858c378fa3b106e49e7bc9964d9d2
xuyang <1006351809@qq.com>
2023-05-25 22:05:29 +0800

    习题1.1和习题1.2的答案

---
+++ 1_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+main()
+{
+	/*故意遗漏分号*/
+	printf("hello world\n")		
+}
---
+++ 1_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main()
+{
+	printf("hello world\y");
+	printf("hello world\g");
+	printf("hello world\f");
+}
