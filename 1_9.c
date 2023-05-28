72c3bf3a071a80dd0144eedaebd428ed0077bab1
xuyang <1006351809@qq.com>
2023-05-28 21:29:17 +0800

    还是有一些难度的

---
+++ 1_9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define NONBLANK 'a'
+
+int main()
+{
+	int c,lastc;
+	lastc = NONBLANK;
+
+	while((c = getchar()) != EOF)
+	{
+		if(c != ' ' || lastc != ' ')
+			putchar(c);
+		lastc = c;
+	}
+}
